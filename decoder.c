16adf3041b9978b1d5eaa9a2311de2cf8fada889
natali <ermolina_natali@bk.ru>
2020-12-20 20:53:01 +0300

    Add decoder.c

---
+++ decoder.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include "functions.h"
+#include "cypher.h"
+
+int main(int argc, char* argv[]) {
+	if (argc != 4 || argc != 1) {
+		printf("Error: invalid number of arguments");
+		exit(1);
+	}
+	if (argc == 1) {
+		printf("Choose decoding method: 1 - caesar, 2 - xor");
+		int n;
+		scanf("%d", &n);
+		printf("String:");
+		char* str = malloc(100 * sizeof(char));
+		scanf("%100s", str);
+		str = (char*)malloc((strlen(str) + 1) * sizeof(char));
+		printf("Key:");
+		int offset;
+		scanf("%d", &offset);
+		if (n == 1) {
+			mutableToLower(str);
+			mutableCaesar(str, -offset);
+			printf("Decoded string: %s", str);
+		}
+		if (n == 2) {
+			mutableToLower(str);
+			mutableXor(str, key);
+			printf("Decoded string: %s", str);
+		}
+		free(str);
+	}
+	if (argc == 4) {
+		if (strcmp(argv[1], "caesar") == 0) {
+			char* str = malloc((strlen(argv[2]) + 1) * sizeof(char));
+			strcpy(argv[2], str);
+			mutableStrip(str);
+			mutableToLower(str);
+ 			int offset;
+			if (isStringNumber(argv[3]) == true) {
+				offset = atoi(argv[3]);
+			}
+			else {
+				printf("Offset must be an integer");
+				exit(1);
+			}
+			mutableCaesar(str, -offset);
+			printf("Decoded string: %s", str);
+			free(str);
+		}
+		if (strcmp(argv[1], "xor") == 0) {
+      			char* str = malloc((strlen(argv[2]) + 1) * sizeof(char));		
+			strcpy(argv[2], str);
+			char* key = malloc((strlen(argv[3]) + 1) * sizeof(char));
+			strcpy(argv[3], key);
+			mutableStrip(str);
+			mutableToLower(str);
+			mutableXor(str, key);
+			printf("Decoded string: %s", str);
+			free(str);
+			free(key);
+		}
+	return 0;
+}
