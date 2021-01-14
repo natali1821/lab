7bb066bdae052ce75c0e118344e8cd86edcc8d2e
natali1821 <ermolina_natali@bk.ru>
2021-01-14 16:04:33 +0300

    Fix decoder.c

--- decoder.c
+++ decoder.c
@@ -14,24 +14,24 @@ int main(int argc, char* argv[]) {
 		printf("Choose decoding method: 1 - caesar, 2 - xor ");
 		int n;
 		scanf("%d", &n);
-        if (n != 1 && n != 2) {
-            printf("Incorrect data");
-            exit(1);
-        }
+		if (n != 1 && n != 2) {
+			printf("Incorrect data");
+			exit(1);
+		}
 		printf("String:");
 		char* str = malloc(100 * sizeof(char));
 		scanf("%100s", str);
 		printf("Key:");
 		if (n == 1) {
-            int offset;
-            scanf("%d", &offset);
-            mutableToLower(str);
+			int offset;
+			scanf("%d", &offset);
+			mutableToLower(str);
 			mutableCaesarDecoder(str, offset);
 			printf("Decoded string: %s", str);
 		}
 		if (n == 2) {
-            char* key = malloc(50 * sizeof(char));
-            scanf("%50s", key);
+			char* key = malloc(50 * sizeof(char));
+			scanf("%50s", key);
 			mutableToLower(str);
 			mutableXorDecoder(str, key);
 			free(key);
@@ -40,32 +40,32 @@ int main(int argc, char* argv[]) {
 		free(str);
 	}
 	if (argc == 4) {
-        char* str = malloc((strlen(argv[2]) + 1) * sizeof(char));
-        strcpy(str, argv[2]);
-        mutableStrip(str);
-        mutableToLower(str);
-        if (strcmp(argv[1], "--caesar") == 0) {
-            int offset;
-            if (isStringNumber(argv[3]) == true) {
-                offset = atoi(argv[3]);
-            }
-            else {
-                printf("Offset must be an integer");
-                exit(1);
-            }
-            mutableCaesarDecoder(str, offset);
-            printf("Decoded string: %s", str);
-        }
-        if (strcmp(argv[1], "--xor") == 0) {
-            char* key = malloc((strlen(argv[3]) + 1) * sizeof(char));
-            strcpy(key, argv[3]);
-            mutableStrip(str);
-            mutableToLower(str);
-            mutableXorDecoder(str, key);
-            printf("Decoded string: %s", str);
-            free(key);
-            }
-        free(str);
+		char* str = malloc((strlen(argv[2]) + 1) * sizeof(char));
+		strcpy(str, argv[2]);
+		mutableStrip(str);
+		mutableToLower(str);
+		if (strcmp(argv[1], "--caesar") == 0) {
+			int offset;
+			if (isStringNumber(argv[3]) == true) {
+				offset = atoi(argv[3]);
+			}
+			else {
+				printf("Offset must be an integer");
+				exit(1);
+			}
+			mutableCaesarDecoder(str, offset);
+			printf("Decoded string: %s", str);
+		}
+		if (strcmp(argv[1], "--xor") == 0) {
+			char* key = malloc((strlen(argv[3]) + 1) * sizeof(char));
+			strcpy(key, argv[3]);
+			mutableStrip(str);
+			mutableToLower(str);
+			mutableXorDecoder(str, key);
+			printf("Decoded string: %s", str);
+			free(key);
+		}
+		free(str);
 	}
 	return 0;
 }
